7cab098ade69664074428dc2245234a8ce69a22a
TheDevShredder <ragdev357@gmail.com>
2018-01-16 10:49:13 +0800

    created a text to speech features

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  text: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
 
   }
 
+  async sayText():Promise<any> {
+    try {
+      await this.tts.speak(this.text);
+      console.log('Success!')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
